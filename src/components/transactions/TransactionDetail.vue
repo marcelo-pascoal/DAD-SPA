6093c8cc1cf86602d1c1499b6cce163ff2d91f7f
marcelo-pascoal <132770891+marcelo-pascoal@users.noreply.github.com>
2023-12-03 01:10:52 +0000

    New Transaction (form start)

--- src/components/transactions/TransactionDetail.vue
+++ src/components/transactions/TransactionDetail.vue
@@ -2,7 +2,10 @@
   import { ref, watch, computed } from 'vue'
   import { useCategoriesStore } from "../../stores/categories.js"
 
+  import { useUserStore } from "../../stores/user.js"
+
   const categoriesStore = useCategoriesStore()
+  const userStore = useUserStore()
   
   const props = defineProps({
     transaction: {
@@ -53,6 +56,24 @@
   >
     <h3 class="mt-5 mb-3">{{ transactionTitle }}</h3>
     <hr>
+
+    <div class="d-flex flex-wrap justify-content-between">
+      <div class="mb-3 ms-xs-3 flex-grow-1">
+        <label for="inputType" class="form-label" >Type</label>
+        <div>
+          <button class="btn btn-xs btn-light" disabled>
+                <i class="bi bi-xs bi-cash" v-if="!editingTransaction.value && userStore.userType === 'A' || editingTransaction.value === 'C'" ></i>
+                <i class="bi bi-xs bi-cart3" v-else></i>
+          </button>
+        </div>
+      </div>
+      <div class="mb-3 me-3 flex-grow-1">
+        <label for="inputValue" class="form-label">Value</label>
+        <input type="number" class="form-control" :class="{ 'is-invalid': errors ? errors['value'] : false }"
+          id="inputValue" placeholder=0 required v-model="editingTransaction.value">
+        <field-error-message :errors="errors" fieldName="value"></field-error-message>
+      </div>
+    </div>
 
   </form>
 </template>
